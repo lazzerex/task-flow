e24c9350391d6d5acc74effb502802796e7d7381
HP <nahmsix2005@gmail.com>
2025-06-10 23:47:17 +0700

    fix style sheet problem

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TaskFlow - Manage Your Tasks Efficiently</title>
     <!-- Custom Styles -->
-    <link rel ="stylesheet" href="{{ asset('css/app.css') }}">
+    
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
